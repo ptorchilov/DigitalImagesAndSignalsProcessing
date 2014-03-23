7564e3ad90748d08d9deacc7d9b104bb2cf28b95
p.torchilov <p.torchilov@gmail.com>
2014-03-24 00:14:30 +0300

    Add noise area.

--- Lab01_HopfieldNN/ApplicationForm.Designer.cs
+++ Lab01_HopfieldNN/ApplicationForm.Designer.cs
@@ -31,16 +31,29 @@
             this.pictureBoxA = new System.Windows.Forms.PictureBox();
             this.pictureBoxB = new System.Windows.Forms.PictureBox();
             this.pictureBoxC = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonNoise = new System.Windows.Forms.Button();
+            this.numericUpDownNoise = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNoise)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBoxA
             // 
+            this.pictureBoxA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxA.ImageLocation = "D:\\work\\VS\\sem8\\cosii\\DigitalImagesAndSignalsProcessing\\Pictures Sources\\original" +
                 "\\A\\A.bmp";
-            this.pictureBoxA.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxA.Location = new System.Drawing.Point(17, 19);
             this.pictureBoxA.Name = "pictureBoxA";
             this.pictureBoxA.Size = new System.Drawing.Size(260, 260);
             this.pictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -49,9 +62,10 @@
             // 
             // pictureBoxB
             // 
+            this.pictureBoxB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxB.ImageLocation = "D:\\work\\VS\\sem8\\cosii\\DigitalImagesAndSignalsProcessing\\Pictures Sources\\original" +
                 "\\B\\B.bmp";
-            this.pictureBoxB.Location = new System.Drawing.Point(338, 12);
+            this.pictureBoxB.Location = new System.Drawing.Point(337, 19);
             this.pictureBoxB.Name = "pictureBoxB";
             this.pictureBoxB.Size = new System.Drawing.Size(260, 260);
             this.pictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -60,28 +74,120 @@
             // 
             // pictureBoxC
             // 
+            this.pictureBoxC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBoxC.ImageLocation = "D:\\work\\VS\\sem8\\cosii\\DigitalImagesAndSignalsProcessing\\Pictures Sources\\original" +
                 "\\C\\C.bmp";
-            this.pictureBoxC.Location = new System.Drawing.Point(672, 12);
+            this.pictureBoxC.Location = new System.Drawing.Point(644, 19);
             this.pictureBoxC.Name = "pictureBoxC";
             this.pictureBoxC.Size = new System.Drawing.Size(260, 260);
             this.pictureBoxC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBoxC.TabIndex = 2;
             this.pictureBoxC.TabStop = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.pictureBoxB);
+            this.groupBox1.Controls.Add(this.pictureBoxC);
+            this.groupBox1.Controls.Add(this.pictureBoxA);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(920, 313);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sources";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(755, 285);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(32, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "C";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(438, 285);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(32, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "B";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(114, 285);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(32, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "A";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDownNoise);
+            this.groupBox2.Controls.Add(this.buttonNoise);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 332);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(409, 309);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Noise";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(16, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonNoise
+            // 
+            this.buttonNoise.Location = new System.Drawing.Point(282, 217);
+            this.buttonNoise.Name = "buttonNoise";
+            this.buttonNoise.Size = new System.Drawing.Size(120, 28);
+            this.buttonNoise.TabIndex = 8;
+            this.buttonNoise.Text = "Generate";
+            this.buttonNoise.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownNoise
+            // 
+            this.numericUpDownNoise.Location = new System.Drawing.Point(283, 153);
+            this.numericUpDownNoise.Name = "numericUpDownNoise";
+            this.numericUpDownNoise.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownNoise.TabIndex = 9;
+            // 
             // ApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(944, 682);
-            this.Controls.Add(this.pictureBoxC);
-            this.Controls.Add(this.pictureBoxB);
-            this.Controls.Add(this.pictureBoxA);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "ApplicationForm";
-            this.Text = "ApplicationForm";
+            this.Text = "Lab 01 - Hopfield neural network";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNoise)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -91,6 +197,14 @@
         private System.Windows.Forms.PictureBox pictureBoxA;
         private System.Windows.Forms.PictureBox pictureBoxB;
         private System.Windows.Forms.PictureBox pictureBoxC;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonNoise;
+        private System.Windows.Forms.NumericUpDown numericUpDownNoise;
     }
 }
 
