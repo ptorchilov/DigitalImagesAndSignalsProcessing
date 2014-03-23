afaae228105f54dd32b5b961e8e79d3605dfe925
p.torchilov <p.torchilov@gmail.com>
2014-03-24 00:25:57 +0300

    Add picture container.

---
+++ Lab01_HopfieldNN/PictureContainer.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PictureContainer.cs" company="BSUIR">
+// Pavel Torchilov
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lab01_HopfieldNN
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Class for store pirtures in bitmap
+    /// </summary>
+    public class PictureContainer
+    {
+        public Bitmap Picture { get; private set; }
+
+        public PictureContainer(String path)
+        {
+            Picture = new Bitmap(path);
+        }
+    }
+}
