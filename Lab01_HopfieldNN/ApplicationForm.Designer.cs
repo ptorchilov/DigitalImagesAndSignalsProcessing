299877dcafd69ae1a090643d1d3be45211e6d763
p.torchilov <p.torchilov@gmail.com>
2014-03-23 23:48:29 +0300

    Add class for pictures path.

--- Lab01_HopfieldNN/ApplicationForm.Designer.cs
+++ Lab01_HopfieldNN/ApplicationForm.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBoxA = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxC = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBoxA
+            // 
+            this.pictureBoxA.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxA.Name = "pictureBoxA";
+            this.pictureBoxA.Size = new System.Drawing.Size(260, 260);
+            this.pictureBoxA.TabIndex = 0;
+            this.pictureBoxA.TabStop = false;
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(338, 12);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(260, 260);
+            this.pictureBoxB.TabIndex = 1;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // pictureBoxC
+            // 
+            this.pictureBoxC.Location = new System.Drawing.Point(672, 12);
+            this.pictureBoxC.Name = "pictureBoxC";
+            this.pictureBoxC.Size = new System.Drawing.Size(260, 260);
+            this.pictureBoxC.TabIndex = 2;
+            this.pictureBoxC.TabStop = false;
             // 
             // ApplicationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(944, 682);
+            this.Controls.Add(this.pictureBoxC);
+            this.Controls.Add(this.pictureBoxB);
+            this.Controls.Add(this.pictureBoxA);
             this.Name = "ApplicationForm";
             this.Text = "ApplicationForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxA;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxC;
     }
 }
 
---
+++ Lab01_HopfieldNN/PicturesContainer.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PicturesContainer.cs" company="BSUIR">
+// Pavel Torchilov
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lab01_HopfieldNN
+{
+    using System;
+
+    /// <summary>
+    /// Class for paths to pitures
+    /// </summary>
+    public static class PicturesContainer
+    {
+        public static String PathToA = @"D:\work\VS\sem8\cosii\DigitalImagesAndSignalsProcessing\Pictures Sources\original\A\A.bmp";
+
+        public static String PathToB = @"D:\work\VS\sem8\cosii\DigitalImagesAndSignalsProcessing\Pictures Sources\original\B\B.bmp";
+
+        public static String PathToC = @"D:\work\VS\sem8\cosii\DigitalImagesAndSignalsProcessing\Pictures Sources\original\C\C.bmp";
+    }
+}
