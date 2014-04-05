df892c1e1c4fe4d40993b10fdbf7b1444a0bdd06
p.torchilov <p.torchilov@gmail.com>
2014-04-05 12:46:32 +0300

    Add teach method.

--- Lab03_RBF/ApplicationForm.Designer.cs
+++ Lab03_RBF/ApplicationForm.Designer.cs
@@ -29,16 +29,53 @@
         private void InitializeComponent()
         {
             this.groupBoxSources = new System.Windows.Forms.GroupBox();
+            this.pictureBoxA = new System.Windows.Forms.PictureBox();
+            this.pictureBoxC = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.groupBoxSources.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBoxSources
             // 
+            this.groupBoxSources.Controls.Add(this.pictureBoxB);
+            this.groupBoxSources.Controls.Add(this.pictureBoxC);
+            this.groupBoxSources.Controls.Add(this.pictureBoxA);
             this.groupBoxSources.Location = new System.Drawing.Point(13, 13);
             this.groupBoxSources.Name = "groupBoxSources";
             this.groupBoxSources.Size = new System.Drawing.Size(919, 337);
             this.groupBoxSources.TabIndex = 0;
             this.groupBoxSources.TabStop = false;
             this.groupBoxSources.Text = "Sources";
+            // 
+            // pictureBoxA
+            // 
+            this.pictureBoxA.Location = new System.Drawing.Point(7, 20);
+            this.pictureBoxA.Name = "pictureBoxA";
+            this.pictureBoxA.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxA.TabIndex = 0;
+            this.pictureBoxA.TabStop = false;
+            this.pictureBoxA.ImageLocation = PicturesPath.PathToOriginalA + "1.bmp";
+            // 
+            // pictureBoxC
+            // 
+            this.pictureBoxC.Location = new System.Drawing.Point(663, 20);
+            this.pictureBoxC.Name = "pictureBoxC";
+            this.pictureBoxC.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxC.TabIndex = 1;
+            this.pictureBoxC.TabStop = false;
+            this.pictureBoxC.ImageLocation = PicturesPath.PathToOriginalC + "1.bmp";
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(328, 20);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxB.TabIndex = 2;
+            this.pictureBoxB.TabStop = false;
+            this.pictureBoxB.ImageLocation = PicturesPath.PathToOriginalB + "1.bmp";
             // 
             // ApplicationForm
             // 
@@ -48,6 +85,10 @@
             this.Controls.Add(this.groupBoxSources);
             this.Name = "ApplicationForm";
             this.Text = "Lab 03 - RBF";
+            this.groupBoxSources.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,6 +96,9 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBoxSources;
+        private System.Windows.Forms.PictureBox pictureBoxA;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxC;
     }
 }
 
--- Lab03_RBF/RBFHepler.cs
+++ Lab03_RBF/RBFHepler.cs
@@ -6,6 +6,9 @@
 
 namespace Lab03_RBF
 {
+    using System;
+    using System.Collections.Generic;
+
     /// <summary>
     /// Class for BRF teaching and recognizing.
     /// </summary>
@@ -13,6 +16,11 @@ namespace Lab03_RBF
     public class RBFHepler
     {
         #region Private Fields
+
+        /// <summary>
+        /// The random
+        /// </summary>
+        private static readonly Random Random = new Random();
 
         /// <summary>
         /// The RBF
@@ -47,7 +55,20 @@ namespace Lab03_RBF
         /// <summary>
         /// The threshold of RBF.
         /// </summary>
-        private double[] G; 
+        private double[] G;
+
+        /// <summary>
+        /// The errors.
+        /// </summary>
+        private double[] D;
+
+        private List<double[]> MathWait; 
+
+        #endregion
+
+        #region Public Properties
+
+        public int NumberOfIterations { get; private set; }
 
         #endregion
 
@@ -60,9 +81,14 @@ namespace Lab03_RBF
             this.h = m;
             this.m = m;
 
-            this.W = this.GenerateMatrix(h, m);
+            this.G = new double[this.h];
 
-        } 
+            this.W = this.GenerateMatrix(h, m);
+            this.Y = new double[this.m];
+            
+            this.D = new double[m];
+            MathWait = new List<double[]>(3);
+        }
 
         #endregion
 
@@ -77,8 +103,201 @@ namespace Lab03_RBF
         private double[,] GenerateMatrix(byte width, byte height)
         {
             return new double[width, height];
-        } 
+        }
+
+        /// <summary>
+        /// Initializes the randomly.
+        /// </summary>
+        /// <param name="layer">The layer.</param>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        private void InitializeRandomly(double[,] layer, int width, int height)
+        {
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    layer[i, j] = this.GetRandomValue();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the random value.
+        /// </summary>
+        /// <returns></returns>
+        private double GetRandomValue()
+        {
+            var rand = Random.NextDouble();
+
+            if (Random.Next(0, 2) == 1)
+            {
+                rand = -rand;
+            }
+
+            return rand;
+        }
+
+        private double[] GetMathWait(sbyte[] vector1, sbyte[] vector2, sbyte[] vector3)
+        {
+            var result = new double[n];
+
+            for (var i = 0; i < n; i++)
+            {
+                result[i] = (vector1[i] + vector2[i] + vector3[i]) / 3.0;
+            }
+
+            return result;
+        }
+
+        private double GetSKO(double[] pictureA, double[] pictureB, double[] pictureC)
+        {
+            var sko1 = 0.0;
+            var sko2 = 0.0;
+            var sko3 = 0.0;
+
+            for (var i = 0; i < n; i++)
+            {
+                sko1 += Math.Pow(pictureB[i] - pictureA[i], 2);
+                sko2 += Math.Pow(pictureC[i] - pictureA[i], 2);
+                sko3 += Math.Pow(pictureC[i] - pictureB[i], 2);
+            }
+
+            var sko = Math.Min(Math.Min(sko1, sko2), sko3);
+
+            return sko / 2;
+        }
+
+        private double TeachVector(sbyte[] vector, double sko, byte numberOfPicture)
+        {
+            var maxError = 0.0;
+
+            for (var i = 0; i < m; i++)
+            {
+                G[i] = Math.Exp((-1) * (Evklid_number(vector, MathWait[i])) / Math.Pow(sko, 2));
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                var t = 0.0;
+
+                for (var j = 0; j < h; j++)
+                {
+                    t += W[j, i] * G[j];
+                }
+
+                Y[i] = t;
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                if (i == numberOfPicture)
+                {
+                    D[i] = 1 - Y[i];
+                }
+                else
+                {
+                    D[i] = 0 - Y[i];
+                }
+
+                var abs = Math.Abs(D[i]);
+
+                if (abs > maxError)
+                {
+                    maxError = abs;
+                }
+            }
+
+            this.CorrectW();
+
+            return maxError;
+        }
+
+        private double Evklid_number(sbyte[] vector, double[] mathWait)
+        {
+            var result = 0.0;
+
+            for (var i = 0; i < n; i++)
+            {
+                result += Math.Pow(vector[i] - mathWait[i], 2);
+            }
+
+            return result;
+        }
+
+        private void CorrectW()
+        {
+            for (var i = 0; i < m; i++)
+            {
+                for (var j = 0; j < h; j++)
+                {
+                    W[i, j] += rbf.Alpha * D[j] * G[i];
+                }
+            }
+        }
+
+        private List<sbyte[]> GetAllVectors()
+        {
+            var result = new List<sbyte[]>(m * h);
+            
+            result.Add(rbf.Vectors.VectorA1);
+            result.Add(rbf.Vectors.VectorA2);
+            result.Add(rbf.Vectors.VectorA3);
+            result.Add(rbf.Vectors.VectorB1);
+            result.Add(rbf.Vectors.VectorB2);
+            result.Add(rbf.Vectors.VectorB3);
+            result.Add(rbf.Vectors.VectorC1);
+            result.Add(rbf.Vectors.VectorC2);
+            result.Add(rbf.Vectors.VectorC3);
+
+            return result;
+        }
+
+        private bool IsTeached(double maxError)
+        {
+            return maxError < rbf.Error || NumberOfIterations >= rbf.Timeout;
+        }
 
         #endregion
+
+        public void Teach()
+        {
+            this.InitializeRandomly(W, h, m);
+
+            this.NumberOfIterations = 0;
+            double maxError;
+            var teached = false;
+
+            MathWait.Add(this.GetMathWait(rbf.Vectors.VectorA1, rbf.Vectors.VectorA2, rbf.Vectors.VectorA3));
+            MathWait.Add(this.GetMathWait(rbf.Vectors.VectorB1, rbf.Vectors.VectorB2, rbf.Vectors.VectorB3));
+            MathWait.Add(this.GetMathWait(rbf.Vectors.VectorC1, rbf.Vectors.VectorC2, rbf.Vectors.VectorC3));
+
+            var sko = this.GetSKO(MathWait[0], MathWait[1], MathWait[2]);
+
+            var vectors = new List<sbyte[]>(m * h);
+
+            vectors = this.GetAllVectors();
+
+
+            while (!teached)
+            {
+                this.NumberOfIterations++;
+                maxError = 0.0;
+
+                var error = 0.0;
+
+                for (var i = 0; i < m * h; i++)
+                {
+                    error = this.TeachVector(vectors[i], sko, (byte) (i / 3));
+
+                    if (error > maxError)
+                    {
+                        maxError = error;
+                    }
+                }
+
+                teached = IsTeached(maxError);
+            }
+        }
     }
 }
