bd9af81d8b23f4f98c07e1e560612b8693f9022c
p.torchilov <p.torchilov@gmail.com>
2014-04-05 11:22:06 +0300

    Add RBF helper.

---
+++ Lab03_RBF/RBF.cs
@@ -0,0 +1,70 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RBF.cs" company="BSUIR">
+// Torchilov Pavel
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lab03_RBF
+{
+    /// <summary>
+    /// Class for RBF network representation.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public class RBF
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the alpha.
+        /// </summary>
+        /// <value>
+        /// The alpha.
+        /// </value>
+        public double Alpha { get; private set; }
+
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        /// <value>
+        /// The error.
+        /// </value>
+        public double Error { get; private set; }
+
+        /// <summary>
+        /// Gets the timeout.
+        /// </summary>
+        /// <value>
+        /// The timeout.
+        /// </value>
+        public double Timeout { get; private set; }
+
+        /// <summary>
+        /// Gets the vectors.
+        /// </summary>
+        /// <value>
+        /// The vectors.
+        /// </value>
+        public VectorsContainer Vectors { get; private set; } 
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RBF"/> class.
+        /// </summary>
+        /// <param name="alpha">The alpha.</param>
+        /// <param name="error">The error.</param>
+        /// <param name="timeout">The timeout.</param>
+        /// <param name="vectors">The vectors.</param>
+        public RBF(double alpha, double error, double timeout, VectorsContainer vectors)
+        {
+            Alpha = alpha;
+            Error = error;
+            Timeout = timeout;
+            Vectors = vectors;
+        } 
+
+        #endregion
+    }
+}
---
+++ Lab03_RBF/RBFHepler.cs
@@ -0,0 +1,84 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RBFHepler.cs" company="BSUIR">
+// Torchilov Pavel
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Lab03_RBF
+{
+    /// <summary>
+    /// Class for BRF teaching and recognizing.
+    /// </summary>
+    // ReSharper disable once InconsistentNaming
+    public class RBFHepler
+    {
+        #region Private Fields
+
+        /// <summary>
+        /// The RBF
+        /// </summary>
+        private RBF rbf;
+
+        /// <summary>
+        /// The number on neurons.
+        /// </summary>
+        private byte n;
+
+        /// <summary>
+        /// The number of RBF neurons.
+        /// </summary>
+        private byte h;
+
+        /// <summary>
+        /// The number of output neurons.
+        /// </summary>
+        private byte m;
+
+        /// <summary>
+        /// The output layer.
+        /// </summary>
+        private double[,] W;
+
+        /// <summary>
+        /// The threshold of the hidden layer.
+        /// </summary>
+        private double[] Y;
+
+        /// <summary>
+        /// The threshold of RBF.
+        /// </summary>
+        private double[] G; 
+
+        #endregion
+
+        #region Constructors
+
+        public RBFHepler(RBF rbf, byte n, byte m)
+        {
+            this.rbf = rbf;
+            this.n = n;
+            this.h = m;
+            this.m = m;
+
+            this.W = this.GenerateMatrix(h, m);
+
+        } 
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Generates the matrix.
+        /// </summary>
+        /// <param name="width">The width.</param>
+        /// <param name="height">The height.</param>
+        /// <returns></returns>
+        private double[,] GenerateMatrix(byte width, byte height)
+        {
+            return new double[width, height];
+        } 
+
+        #endregion
+    }
+}
